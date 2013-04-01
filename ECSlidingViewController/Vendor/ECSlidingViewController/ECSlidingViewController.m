dcf837921b77353e9bda3acec0d1556587c34ac9
Jeff Arena <jeff@gateguruapp.com>
2013-04-01 18:52:40 -0400

    Fix bug preventing panning in cases where one under view controller is nil.

--- ECSlidingViewController/Vendor/ECSlidingViewController/ECSlidingViewController.m
+++ ECSlidingViewController/Vendor/ECSlidingViewController/ECSlidingViewController.m
@@ -262,7 +262,12 @@ NSString *const ECSlidingViewTopDidReset             = @"ECSlidingViewTopDidRese
       newCenterPosition = self.resettedCenter;
     }
     
-    BOOL newCenterPositionIsOutsideAnchor = newCenterPosition < self.anchorLeftTopViewCenter || self.anchorRightTopViewCenter < newCenterPosition;
+    BOOL newCenterPositionIsOutsideAnchor = (self.underRightViewController &&
+                                             self.anchorLeftTopViewCenter != NSNotFound &&
+                                             newCenterPosition < self.anchorLeftTopViewCenter) ||
+                                            (self.underLeftViewController &&
+                                             self.anchorLeftTopViewCenter != NSNotFound &&
+                                             self.anchorRightTopViewCenter < newCenterPosition);
     
     if ((newCenterPositionIsOutsideAnchor && self.shouldAllowPanningPastAnchor) || !newCenterPositionIsOutsideAnchor) {
       [self topViewHorizontalCenterWillChange:newCenterPosition];
